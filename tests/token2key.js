5e06f1305c33b0035207554bb3e6b5f19b8304e9
snOOrz <petersun@live.com>
2016-09-06 21:39:06 +0800

    Add files via upload

--- tests/token2key.js
+++ tests/token2key.js
@@ -1,8 +1,16 @@
 const crypto = require('crypto');
-const password = ['0987654321qwerty', 'abcdefghABCDEFGH'];
+const algorithm = 'aes-128-cbc';
+const password = 'ojhd60q2xchzvi9k';
 const iv = Buffer.from([0x17, 0x99, 0x6d, 0x09, 0x3d, 0x28, 0xdd, 0xb3, 0xba, 0x69, 0x5a, 0x2e, 0x6f, 0x58, 0x56, 0x2e]);
-const token = '1234567890abcdef';
-for (var index in password) {
-  const cipher = crypto.createCipheriv('aes-128-cbc', password[index], iv);
-  console.log(cipher.update(token, 'utf8', 'hex'));
-}
+const token = 'pxbliBmIZ2ncfHsC';
+const cipher = crypto.createCipheriv(algorithm, password, iv);
+var encrypted = cipher.update(token, 'ascii', 'hex');
+console.log(encrypted);
+// encrypted += cipher.final('hex');
+// console.log(encrypted);
+
+const decipher = crypto.createDecipheriv(algorithm, password, iv);
+decipher.setAutoPadding(false);
+var decrypted = decipher.update(encrypted, 'hex', 'ascii');
+decrypted += decipher.final('ascii');
+console.log(decrypted);
